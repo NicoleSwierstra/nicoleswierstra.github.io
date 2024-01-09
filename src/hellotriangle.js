b6575046e22a95b539273f217a28030f5dcbd524
= <=>
2024-01-09 00:14:35 -0800

    psuh :)

--- src/hellotriangle.js
+++ src/hellotriangle.js
@@ -1,60 +1,10 @@
 var canvas = document.getElementById('my_Canvas');
 var gl = canvas.getContext('experimental-webgl');
 
+import { getShaderFrom } from "/modules/shader.js";
+
 canvas.width = document.body.clientWidth;
 canvas.height = canvas.width * 0.5625 
-
-function getShaderFrom(vert, frag){
-    var vertShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertShader, vert);
-    gl.compileShader(vertShader);
-    
-    var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragShader, frag);
-    gl.compileShader(fragShader);
-
-    var shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vertShader);
-    gl.attachShader(shaderProgram, fragShader);
-    gl.linkProgram(shaderProgram);
-    
-    return shaderProgram;
-}
-
-function getShaderFromFile(path){
-    let fs = new FileReaderSync();
-    var allContents = await fs.readFileSync('test.json', 'utf-8');
-
-    var shaders = ["", ""];
-    var type = 0;
-    allContents.split(/\r?\n/).forEach((line) => {
-        if (line.startsWith("#shader")) {
-            if (line.includes("vertex")) {
-                type = 0;
-            }
-            else if (line.includes("geometry")) {
-                type = -1;
-            }
-            else if (line.includes("fragment")) {
-                type = 1;
-            }
-        }
-        else if(type != ShaderType.NONE) {
-            shaders[type] += line + '\n';
-        }
-    });
-    return getShaderFrom(shaders[0], shaders[1]); 
-}
-
-class Shader{
-    constructor(filepath){
-        this.id = getShaderFromFile(filepath);
-    }
-
-    bind(){
-        gl.useProgram(this.id);
-    }
-}
 
 function hexToRgb(hex) {
     var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
@@ -63,131 +13,94 @@ function hexToRgb(hex) {
         g: parseInt(result[2], 16),
         b: parseInt(result[3], 16)
     } : null;
-}  
+}
 
-var vertices = [
-    -1,-1,-1,  1,-1,-1,  1, 1,-1, -1, 1,-1,
-    -1,-1, 1,  1,-1, 1,  1, 1, 1, -1, 1, 1,
-    -1,-1,-1, -1, 1,-1, -1, 1, 1, -1,-1, 1,
-     1,-1,-1,  1, 1,-1,  1, 1, 1,  1,-1, 1,
-    -1,-1,-1, -1,-1, 1,  1,-1, 1,  1,-1,-1,
-    -1, 1,-1, -1, 1, 1,  1, 1, 1,  1, 1,-1, 
-];
+const roadx = 11;
+const roady = 200;
+const scalex = 2.5;
+const scaley = 60.0;
+const mountainx = 20;
+const sunres = 64;
+const end_dist = scaley * 10;
+var dist_traveled = 0.0;
 
-var colors = [
-    5, 3, 7,  5, 3, 7,  5, 3, 7,  5, 3, 7,
-    1, 1, 3,  1, 1, 3,  1, 1, 3,  1, 1, 3,
-    0, 0, 1,  0, 0, 1,  0, 0, 1,  0, 0, 1,
-    1, 0, 0,  1, 0, 0,  1, 0, 0,  1, 0, 0,
-    1, 1, 0,  1, 1, 0,  1, 1, 0,  1, 1, 0,
-    0, 1, 0,  0, 1, 0,  0, 1, 0,  0, 1, 0
-];
+const TAU = 6.28318530717958623199592693709
+const rand_div = 0.000030518509476
 
-var indices = [
-    0,1,2,  0,2,3, 4,5,6, 4,6,7,
-    8,9,10, 8,10,11, 12,13,14, 12,14,15,
-   16,17,18, 16,18,19, 20,21,22, 20,22,23 
-];
+const rot = glm.rotate(glm.mat4(1.), glm.radians(-65.), glm.vec3(1.0, 0.0, 0.0));
+let trans = glm.mat4(1.0);
+let trans2 = glm.mat4(1.0);
+let pos = glm.vec3(0.0, 0.0, -1.0);
+let suntrans = glm.translate(rot, glm.vec3(0.0, (5 * scaley) + end_dist, 0.0));
+
+console.log(pos);
+
+let sunverts = [0.0, 0.0, 0.0];
+let sunindex = [];
+
+for (let i = 0; i < sunres; i++) {
+    let h = i * TAU;
+    sunverts.push(Math.cos(h / sunres) * 130.0, 0.0, Math.sin(h / sunres) * 115.0);
+}
+for (let i = 1; i <= sunres; i++) {
+    sunindex.push(0, i, i % sunres ? i + 1 : 1);
+}
 
 // Create and store data into vertex buffer
-var vertex_buffer = gl.createBuffer ();
-gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-
-// Create and store data into color buffer
-var color_buffer = gl.createBuffer ();
-gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
-gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+var sun_vb = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, sun_vb);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(sunverts), gl.STATIC_DRAW);
 
 // Create and store data into index buffer
-var index_buffer = gl.createBuffer ();
-gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
-gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+var sun_ib = gl.createBuffer();
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, sun_ib);
+gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(sunindex), gl.STATIC_DRAW);
 
-var shader = getShaderFromFile("test.shader");
+// Create and store data into vertex buffer
+var road_vb = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, sun_vb);
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(sunverts), gl.STATIC_DRAW);
 
-/* ====== Associating attributes to vertex shader =====*/
-var Pmatrix = gl.getUniformLocation(shaderProgram, "Pmatrix");
-var Vmatrix = gl.getUniformLocation(shaderProgram, "Vmatrix");
-var Mmatrix = gl.getUniformLocation(shaderProgram, "Mmatrix");
+// Create and store data into index buffer
+var road_ib = gl.createBuffer();
+gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, sun_ib);
+gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(sunindex), gl.STATIC_DRAW);
 
-gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
-var position = gl.getAttribLocation(shaderProgram, "position");
-gl.vertexAttribPointer(position, 3, gl.FLOAT, false,0,0) ;
+let sunvert = '\
+attribute vec3 position; \
+\
+uniform mat4 u_MVP; \
+uniform mat4 trans; \
+varying float l; \
+\
+void main(void) { \
+    gl_Position = u_MVP * trans * vec4(position, 1.0); \
+    l = position.z / 160.0; \
+}';
+
+let sunfrag = '\
+precision mediump float;\
+varying float l;\
+\
+void main(void) {\
+    gl_FragColor = mix(vec4(1.0, 0.0, 0.0, 1.0), vec4(1.0, 1.0, 0.0, 1.0), l);\
+}';
+
+var sunShader = getShaderFrom(sunvert, sunfrag);
+
+gl.bindBuffer(gl.ARRAY_BUFFER, sun_vb);
+var position = gl.getAttribLocation(sunShader, "position");
+gl.vertexAttribPointer(position, 3, gl.FLOAT, false,0,0);
 
 // Position
 gl.enableVertexAttribArray(position);
-gl.bindBuffer(gl.ARRAY_BUFFER, color_buffer);
-var color = gl.getAttribLocation(shaderProgram, "color");
-gl.vertexAttribPointer(color, 3, gl.FLOAT, false,0,0) ;
+gl.useProgram(sunShader);
 
-// Color
-gl.enableVertexAttribArray(color);
-gl.useProgram(shaderProgram);
+var proj_matrix = glm.perspective(glm.radians(90.0), canvas.width / canvas.height, 0.1, 5000.0);
 
-/*==================== MATRIX =====================*/
-
-function get_projection(angle, a, zMin, zMax) {
-var ang = Math.tan((angle*.5)*Math.PI/180);//angle*.5
-return [
-    0.5/ang, 0 , 0, 0,
-    0, 0.5*a/ang, 0, 0,
-    0, 0, -(zMax+zMin)/(zMax-zMin), -1,
-    0, 0, (-2*zMax*zMin)/(zMax-zMin), 0 
-];
-}
-
-var proj_matrix = get_projection(40, canvas.width/canvas.height, 1, 100);
-
-var mov_matrix = [1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1];
-var view_matrix = [1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1];
-
-// translating z
-view_matrix[14] = view_matrix[14]-6;//zoom
-
-/*==================== Rotation ====================*/
-
-function rotateZ(m, angle) {
-    var c = Math.cos(angle);
-    var s = Math.sin(angle);
-    var mv0 = m[0], mv4 = m[4], mv8 = m[8];
-
-    m[0] = c*m[0]-s*m[1];
-    m[4] = c*m[4]-s*m[5];
-    m[8] = c*m[8]-s*m[9];
-
-    m[1]=c*m[1]+s*mv0;
-    m[5]=c*m[5]+s*mv4;
-    m[9]=c*m[9]+s*mv8;
-}
-
-function rotateX(m, angle) {
-    var c = Math.cos(angle);
-    var s = Math.sin(angle);
-    var mv1 = m[1], mv5 = m[5], mv9 = m[9];
-
-    m[1] = m[1]*c-m[2]*s;
-    m[5] = m[5]*c-m[6]*s;
-    m[9] = m[9]*c-m[10]*s;
-
-    m[2] = m[2]*c+mv1*s;
-    m[6] = m[6]*c+mv5*s;
-    m[10] = m[10]*c+mv9*s;
-}
-
-function rotateY(m, angle) {
-    var c = Math.cos(angle);
-    var s = Math.sin(angle);
-    var mv0 = m[0], mv4 = m[4], mv8 = m[8];
-
-    m[0] = c*m[0]+s*m[2];
-    m[4] = c*m[4]+s*m[6];
-    m[8] = c*m[8]+s*m[10];
-
-    m[2] = c*m[2]-s*mv0;
-    m[6] = c*m[6]-s*mv4;
-    m[10] = c*m[10]-s*mv8;
-}
+gl.uniformMatrix4fv(gl.getUniformLocation(sunShader, "u_MVP"), false, proj_matrix.array);
+gl.uniformMatrix4fv(gl.getUniformLocation(sunShader, "u_MVP"), false, proj_matrix.array);
+gl.uniformMatrix4fv(gl.getUniformLocation(sunShader, "trans"), false, suntrans.array);
 
 /*================= Drawing ===========================*/
 var time_old = 0;
@@ -195,24 +108,22 @@ var time_old = 0;
 gl.enable(gl.DEPTH_TEST);
 gl.depthFunc(gl.LEQUAL);
 var bgcol = hexToRgb(getComputedStyle(canvas).getPropertyValue('--d_dark-grey'));
+
 gl.clearColor(bgcol.r/256.0, bgcol.g/256.0, bgcol.b/256.0, 1);
 gl.clearDepth(1.0);
-
 var animate = function(time) {
-
-    var dt = time-time_old;
-    rotateZ(mov_matrix, dt*0.005);//time
-    rotateY(mov_matrix, dt*0.002);
-    rotateX(mov_matrix, dt*0.003);
     time_old = time;
-
     gl.viewport(0.0, 0.0, canvas.width, canvas.height);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.uniformMatrix4fv(Pmatrix, false, proj_matrix);
-    gl.uniformMatrix4fv(Vmatrix, false, view_matrix);
-    gl.uniformMatrix4fv(Mmatrix, false, mov_matrix);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, index_buffer);
-    gl.drawElements(gl.TRIANGLES, indices.length, gl.UNSIGNED_SHORT, 0);
+
+    trans = glm.translate(rot, pos);
+    trans2 = glm.translate(rot, pos['+'](glm.vec3(0, scaley, 0)));
+
+    gl.uniformMatrix4fv(gl.getUniformLocation(sunShader, "trans"), false, suntrans.array);
+
+    gl.useProgram(sunShader);
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, sun_ib);
+    gl.drawElements(gl.TRIANGLES, sunindex.length, gl.UNSIGNED_SHORT, 0);
 
     window.requestAnimationFrame(animate);
 }
---
+++ src/modules/shader.js
@@ -0,0 +1,64 @@
+export { getShaderFrom, getShaderFromFile, Shader };
+const fs = require('fs')
+
+function getShaderFrom(vert, frag){
+    var vertShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertShader, vert);
+    gl.compileShader(vertShader);
+
+    var compiled = gl.getShaderParameter(vertShader, gl.COMPILE_STATUS);
+    console.log('Shader compiled successfully: ' + compiled);
+    var compilationLog = gl.getShaderInfoLog(vertShader);
+    console.log('Shader compiler log: ' + compilationLog);
+    
+    var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragShader, frag);
+    gl.compileShader(fragShader);
+
+    var compiled = gl.getShaderParameter(fragShader, gl.COMPILE_STATUS);
+    console.log('Shader compiled successfully: ' + compiled);
+    var compilationLog = gl.getShaderInfoLog(fragShader);
+    console.log('Shader compiler log: ' + compilationLog);
+
+    var shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertShader);
+    gl.attachShader(shaderProgram, fragShader);
+    gl.linkProgram(shaderProgram);
+    
+    return shaderProgram;
+}
+
+async function getShaderFromFile(path){
+    let fs = new FileReaderSync();
+    var allContents = await fetch(path).then(x => x.text());
+
+    var shaders = ["", ""];
+    var type = 0;
+    allContents.split(/\r?\n/).forEach((line) => {
+        if (line.startsWith("#shader")) {
+            if (line.includes("vertex")) {
+                type = 0;
+            }
+            else if (line.includes("geometry")) {
+                type = -1;
+            }
+            else if (line.includes("fragment")) {
+                type = 1;
+            }
+        }
+        else if(type != ShaderType.NONE) {
+            shaders[type] += line + '\n';
+        }
+    });
+    return getShaderFrom(shaders[0], shaders[1]); 
+}
+
+class Shader{
+    constructor(filepath){
+        this.id = getShaderFromFile(filepath);
+    }
+
+    bind(){
+        gl.useProgram(this.id);
+    }
+}
